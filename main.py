13a983d5fd5af93a2b2ccefa006cc86207cc9d7f
xEvertonWallace <102435699+xEvertonWallace@users.noreply.github.com>
2023-02-06 21:58:19 -0300

    verificação
    faz a verificação da letra digitada com a palavra descriptografada  e se estiver correto, substitui na palavra criptografada no índice i.

--- main.py
+++ main.py
@@ -29,3 +29,17 @@ while continuacao == 'sim':
      for i in range(0, tam):#verifica o tamanho da varivel tam e adiciona um hífen na variável palCrypt a cada passada pelo for
         palCrypt.append("-")
      print("".join(palCrypt))
+
+     acertou = False
+     vidasPerdidas = 0
+     while acertou == False and vidasPerdidas < vidas:
+          letra = input("Digite uma letra: ").upper()
+          if letra in palDescrypt:
+            for i in range(0, tam):
+               if letra == palDescrypt[i] :#verifica se a letra digitada esta na palDescrypt e se estiver, substitui a letra na palCrypt a partir do índice i
+                    palCrypt[i]= letra
+          else:
+            vidasPerdidas += 1#se a letra nao estiver na palDescrypt soma +1 nas vidas perdidas.
+            print("Essa letra não existe na palavra.")
+          print("".join(palCrypt))
+          acertou = True
