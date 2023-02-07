c0517a0d63333f03dd654627eee9ffdf26b9b8cb
xEvertonWallace <102435699+xEvertonWallace@users.noreply.github.com>
2023-02-06 21:49:23 -0300

    sorteio
    sorteia e armazena uma das palavras da lista a partir do random

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 vidas = int(input("Informe O numero de vidas: "))
 while vidas <= 0:
@@ -6,10 +7,17 @@ qtd = int(input("Digite a quantidade de palavras: "))
 
 while qtd <= 0:
     qtd=int(input('Você digitou uma quantidade de palavras inválida. Tente Novamente.\n'))
-    
+
 listaPalavras = list()
 i = 1
 while i <= qtd:
         palavra = str(input(f"Digite a {i} º palavra:  ")).upper()
         listaPalavras.append(palavra)#adiciona as palavras na lista de palavras.
-        i += 1   
+        i += 1
+
+continuacao = 'sim'
+while continuacao == 'sim':
+    
+    palAleatoria = random.choice(listaPalavras)#sorteia e armazena uma das palavras da lista.
+    palDescrypt = list() 
+    palCrypt = list()
