207c3538995b47a981e52f450fbb64c195eeeaba
xEvertonWallace <102435699+xEvertonWallace@users.noreply.github.com>
2023-02-06 21:42:57 -0300

    start
    recebe o numero de vidas e palavras do usuário e verifica se o número digitado é maior que zero.

---
+++ cacaPalavras.py
@@ -0,0 +1,8 @@
+
+vidas = int(input("Informe O numero de vidas: "))
+while vidas <= 0:
+     vidas=int(input('Você digitou um numero de vidas inválido. Tente Novamente.\n'))
+qtd = int(input("Digite a quantidade de palavras: "))
+
+while qtd <= 0:
+    qtd=int(input('Você digitou uma quantidade de palavras inválida. Tente Novamente.\n'))
